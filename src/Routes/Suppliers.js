cfa91e7cb19aa21909a584cc76865b783b247ddf
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2026-03-12 17:04:53 -0300

    Route Suppliers

--- src/Routes/Suppliers.js
+++ src/Routes/Suppliers.js
@@ -1,16 +1,55 @@
 import { Router } from "express";
+import { Supplier } from "../Models/Suppliers.js";
 import { SupplierController } from "../Controllers/SupplierController";
+
+const supplierController = new SupplierController();
 
 const route = Router();
 
-route.get("/listarfornecedores", SupplierController.listarSuppliers);
+route.get("/listarfornecedores", (req, res) => {
+    res.status(200).json({ message: "Lista de fornecedores." });
+});
 
-route.post("/cadastrarfornecedor", SupplierController.adicionarSupplier)
+route.post("/cadastrarfornecedor", async (req, res) => {
+    const supplier = new Supplier(
+        req.body.nome_fantasia,
+        req.body.cnpj,
+        req.body.contato
+    );
+    const result = await supplierController.adicionarSupplier(supplier);
 
-route.delete("/deletarfornecedor/:id", SupplierController.deletarSupplier);
+    if(result.length > 0){
+        res.status(201).json({ message: "Fornecedor criado com sucesso!" });
+    } else {
+        res.status(404).json({ message: "Erro ao criar fornecedor!" });
+    }
+});
 
-route.put("/atualizarfornecedor", SupplierController.atualizarSupplier);
+route.delete("/deletarfornecedor/:id", async (req, res) => {
+    const result = await supplierController.deletarSupplier(req, res);
+    if(result.length > 0){
+        res.status(200).json({ message: "Fornecedor deletado com sucesso!" });
+    } else {
+        res.status(404).json({ message: "Erro ao deletar fornecedor!" });
+    }
+});
 
-route.get("/findsupplier", SupplierController.findSupllier); //buscar fornecedor pelo cpnj
+route.put("/atualizarfornecedor", async (req, res) => {
+    const result = await supplierController.atualizarSupplier(req, res);
+    if(result.length > 0){
+        res.status(200).json({ message: "Fornecedor atualizado com sucesso!" });
+    } else {
+        res.status(404).json({ message: "Erro ao atualizar fornecedor!" });
+    }
+});
+
+route.get("/findsupplier", async (req, res) => {
+    const result = await supplierController.findSupplier(req, res);
+    if(result.length > 0){
+        res.status(200).json(result);
+    } else {
+        res.status(404).json({ message: "Fornecedor não encontrado!" });
+    }
+});
 
 export default route;
